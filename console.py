366730a0a9b7bd6690f9aa6a8e14892a552618c2
Alsayedaldahawy <alsayedaldahawy@outlook.com>
2024-05-11 05:01:14 +0300

    recovery point

--- console.py
+++ console.py
@@ -5,6 +5,8 @@ console.py module contains the entry point of the command interpreter:
 """
 
 import cmd
+from models.base_model import BaseModel
+from models import storage 
 
 
 class HBNBCommand(cmd.Cmd):
@@ -50,6 +52,37 @@ class HBNBCommand(cmd.Cmd):
         """ Overrides the emptyline method of CMD """
         pass
 
+    def do_create(self, line):
+        if not line:
+            print("** class name missing **")
+        elif line == "BaseModel":
+            obj = BaseModel()
+            storage.save()
+            print(obj.id)
+        else:
+            print("** class doesn't exist **")
+
+    def help_create(self):
+        print("Creates a new instance of BaseModel,\nsaves it (to the JSON file) and prints the id.\nEx: $ create BaseModel")
+
+    def do_show(self, arg):
+
+        args = arg.split()
+
+        if not arg:
+            print("** class name missing **")
+        elif args[0] not in ["BaseModel"]:
+            print("** class doesn't exist **")
+        elif len(args) < 2:
+            print("** instance id missing **")
+        else:
+            if args[1] not in storage._FileStorage__objects:
+                print("** no instance found **")
+            else:
+                print(storage._FileStorage__objects[args[1]])
+
+        
+        
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
