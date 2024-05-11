c4f13665f2faf96c6b902a50829f975a307992fe
Alsayedaldahawy <alsayedaldahawy@outlook.com>
2024-05-11 17:18:16 +0300

    AirBnB_clone update

--- models/user.py
+++ models/user.py
@@ -1,9 +1,19 @@
 #!/usr/bin/python3
 
+"""
+User Module
+
+This module defines the User class with attributes for email, password,\
+    first name, and last name.
+"""
+
 from models.base_model import BaseModel
 
 
 class User(BaseModel):
+    """
+    Class User inherited from BaseModel Class
+    """
     email = ""
     password = ""
     first_name = ""
