12d2cf80f20c1e5e1fa9bfac52060734657ce6d7
Alsayedaldahawy <alsayedaldahawy@outlook.com>
2024-05-11 03:05:45 +0300

    AirBnB_clone update

--- models/__init__.py
+++ models/__init__.py
@@ -2,4 +2,7 @@ from models.engine.file_storage import FileStorage
 
 storage = FileStorage()
 
-storage.reload()
+try:
+    storage.reload()
+except:
+    pass
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,27 +1,38 @@
 #!/usr/bin/python3
 
 import json
+import os
+
 
 class FileStorage:
 
-    def __init__(self):
-        self.__file_path = "file.json"
-        self.__objects = {}
+    __file_path = "file.json"
+    __objects = {}
 
     def all(self):
-        return self.__objects
-    
+        return self.__class__.__objects
+
     def new(self, obj):
-        self.__objects[type(obj).__name__ + "." + obj.id] = obj.to_dict()
-    
+        self.__class__.__objects["{}.{}".format(
+            obj.__class__.__name__, obj.id)] = obj
+
     def save(self):
-        with open(self.__file_path, "w+") as jsonFile:
-            jsonFile.write(json.dumps(self.__objects))
-        
+
+        with open(self.__class__.__file_path, "w") as jsonFile:
+            obj_to_dict_list = []
+            for key, obj in self.__class__.__objects.items():
+                obj_to_dict = {}
+                obj_to_dict[key] = obj.to_dict()
+                obj_to_dict_list.append(obj_to_dict)
+            jsonFile.write(json.dumps(obj_to_dict_list))
+
     def reload(self):
+        from models.base_model import BaseModel
         try:
-            with open(self.__file_path, "r") as jsonFile:
-                self.__objects = json.loads(jsonFile.read())
-        except FileNotFoundError:
+            with open(self.__class__.__file_path, "r") as jsonFile:
+                dicts_to_obj_list = json.loads(jsonFile.read())
+                for dicts in dicts_to_obj_list:
+                    for key, value in dicts.items():
+                        self.__class__.__objects[key] = BaseModel(**value)
+        except:
             pass
-    
