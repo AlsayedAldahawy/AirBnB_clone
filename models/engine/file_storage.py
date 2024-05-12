db9e40843ed24fd8e0a0d4695e082a7f4dcc10d2
Alsayedaldahawy <alsayedaldahawy@outlook.com>
2024-05-12 05:18:23 +0300

    AirBnB_clone update

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,23 +1,57 @@
 #!/usr/bin/python3
 
+"""
+    file_storage.py module contains FileStorage Class
+"""
+
+
 import json
-import os
 
 
 class FileStorage:
+    """
+    FileStorage Class
 
+    This class manages the storage of objects in a JSON file. It provides\
+        methods for saving, loading, and manipulating objects.
+
+    Attributes:
+        __file_path (str): The path to the JSON file where objects are stored.
+        __objects (dict): A dictionary containing object instances, indexed by\
+            their unique keys.
+    """
     __file_path = "file.json"
     __objects = {}
 
     def all(self):
+        """
+        Returns all objects stored in the storage.
+
+        Returns:
+            dict: A dictionary of object instances.
+        """
         return self.__class__.__objects
 
     def new(self, obj):
+        """
+        Adds a new object to the storage.
+
+        Args:
+            obj: An instance of a class.
+
+        Returns:
+            None
+        """
         self.__class__.__objects["{}.{}".format(
             obj.__class__.__name__, obj.id)] = obj
 
     def save(self):
+        """
+        Saves objects to the JSON file.
 
+        Returns:
+            None
+        """
         with open(self.__class__.__file_path, "w") as jsonFile:
             obj_to_dict_list = []
             for key, obj in self.__class__.__objects.items():
@@ -27,6 +61,12 @@ class FileStorage:
             jsonFile.write(json.dumps(obj_to_dict_list))
 
     def reload(self):
+        """
+        Reloads objects from the JSON file.
+
+        Returns:
+            None
+        """
         from models.base_model import BaseModel
         from models.state import State
         from models.city import City
